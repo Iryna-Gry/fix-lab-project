20cb472f7740d14b3a286ce9132bfd129e8a5ce6
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2024-01-28 23:08:59 +0200

    fixes adaptive for mobile and partially tablet

--- apps/client/app/(layouts)/HeroSection.tsx
+++ apps/client/app/(layouts)/HeroSection.tsx
@@ -5,6 +5,7 @@ import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import { useCallback, useState } from 'react'
 
+import { CommonButton } from '../(components)/CommonButton'
 import ArrowImage from '../../public/icons/arrow-down-right.svg'
 import IconGuard from '../../public/icons/icon-guard.svg'
 import IconScredriver from '../../public/icons/icon-screwdriver.svg'
@@ -19,7 +20,6 @@ import MatrixImage from '../../public/icons/pop-matrix.svg'
 import PowerImage from '../../public/icons/pop-power.svg'
 import SmartphoneImage from '../../public/icons/pop-smartphone.svg'
 import HeroPhone from '../../public/images/phone-hero.png'
-import { CommonButton } from '../(components)/CommonButton'
 
 const HeroSlider = dynamic(() => import('./(components)/HeroSlider/HeroSlider'))
 const SuccessSubmitBanner = dynamic(
@@ -131,8 +131,8 @@ export const HeroSection = () => {
     ))
 
   return (
-    <section className='section xl:bg-herp-xl w-full bg-dark-blue bg-hero-xl py-0 md:bg-hero-md lg:bg-hero-lg xl:bg-hero-xl'>
-      <div className='lg relative size-full bg-mscope-img bg-mscope-sm bg-no-repeat pb-[80px] pt-[156px] md:container max-md:pb-[30px] md:bg-mscope-md md:pt-[162px] lg:bg-mscope-lg xl:bg-mscope-xl xl:px-0'>
+    <section className='section xl:bg-herp-xl w-full h-[100vh] bg-dark-blue bg-hero-xl py-0 md:bg-hero-md lg:bg-hero-lg xl:bg-hero-xl'>
+      <div className='relative size-full bg-mscope-img bg-mscope-sm bg-no-repeat pb-[80px] pt-[13vh] md:container max-lg:pb-0 md:pt-[20vh] md:bg-mscope-md lg:pt-[162px] lg:bg-mscope-lg xl:bg-mscope-xl xl:px-0'>
         <Image
           className='absolute left-[51%] top-[58%] max-w-[300px] -translate-x-1/2 -translate-y-1/2 text-white-dis max-xl:left-[48%] max-xl:top-[55%]  max-md:left-[44%] max-md:top-[54%] max-md:hidden  max-[430px]:left-[44%] max-[430px]:top-[54%] max-[430px]:w-[170px]'
           width={246}
@@ -142,29 +142,29 @@ export const HeroSection = () => {
         />
         <div
           data-aos='fade-up'
-          className='relative flex flex-col md:px-4 xl:px-0'
+          className='relative flex flex-col justify-between h-full md:px-4 xl:px-0'
         >
           <div className='mt-[-11px] flex flex-col items-center justify-start px-4 font-exo_2 text-white-dis md:w-[500px] md:items-start md:px-0'>
-            <h2 className='text-3xl font-bold leading-none xl:text-title'>
+            <h2 className=' text-xl md:text-2xl lg:text-3xl font-bold leading-none xl:text-title'>
               Бачимо гаджети
             </h2>
-            <h3 className='text-4xl font-bold leading-none text-mid-blue drop-shadow-banner xl:text-9xl'>
+            <h3 className='text-3xl lg:text-4xl font-bold leading-none text-mid-blue drop-shadow-banner xl:text-9xl'>
               наскрізь
             </h3>
 
-            <p className='mt-[22px] font-inter text-md leading-none tracking-title md:mt-11 md:text-xl md:tracking-normal'>
+            <p className='mt-[22px] text-sm font-inter leading-none tracking-title lg:mt-11 lg:text-xl md:tracking-normal'>
               Мережа студій ремонту твоєї техніки
             </p>
 
-            <div className='mt-[54px] w-full md:mt-[48px] md:w-[320px] xl:w-[336px]'>
+            <div className='mt-[20px] w-[280px] lg:mt-[48px] md:w-[300px] xl:w-[336px]'>
               <CommonButton onClick={toggleCostRepairModal}>
                 Розрахувати вартість ремонту
               </CommonButton>
             </div>
           </div>
 
-          <div className='overflow-x-hidden pt-[424px] md:max-w-[411px] md:pt-[54px]'>
-            <h3 className='heading-none relative mx-3 border-b-2 border-pros-bg pb-[10px] align-top font-exo_2 text-xl tracking-middle text-white-dis md:mx-0'>
+          <div className='overflow-x-hidden md:max-w-[350px] lg:max-w-[411px] lg:pt-[54px]'>
+            <h3 className='heading-none relative mx-3 border-b-2 border-pros-bg pb-[10px] align-top font-exo_2 text-xl md:text-lg lg:text-xl tracking-middle text-white-dis md:mx-0'>
               Найчастіші звернення
               <Image
                 className='absolute right-[2px] top-[calc(50%-4px)] -translate-y-1/2 text-white-dis'
